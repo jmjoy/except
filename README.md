66ebf9319cec9ffb369db05eb821785c79a517a9
jmjoy <jmjoy@apache.org>
2023-06-01 18:10:21 +0800

    Refactor.

--- README.md
+++ README.md
@@ -1,6 +1,8 @@
 # Except
 
-Exception: The only `Error` you deserve.
+The only one `Error`.
+
+**only available in nightly toolchain now.**
 
 ## Why?
 
@@ -35,89 +37,37 @@ application.
 
 *This is just a personal opinion.*
 
-An Error(Exception) actually only contains the following elements:
+An Error actually only contains the following elements:
 
-- `kind`: Auto generated id, used to determine whether the Error(Exception) is a certain type.
-- `code`: For business logic used or sub type detected.
-- `message`: String describing the Error(Exception).
-- `data`: Optional Error(Exception) data.
-- `backtrace`: Error(Exception) call stack.
-- `source`: Optional previous Error(Exception).
+- `type`: Auto generated id, used to determine whether the Error is a certain type.
+- `sub_type`: Auto generated id, used to determine whether the Error is a certain sub type, used to supplement type.
+- `message`: String describing the Error.
+- `data`: Optional Error data.
+- `backtrace`: Error call stack.
+- `source`: Optional previous Error.
 
 For Rust, the `message`, ~~`backtrace`,~~ `source` already exists in `std::error::Error`.
 
-Then I prefer to auto generate the `code(kind)`, I think `TypeId` is a solution.
+Then I prefer to auto generate the `type`, I think `TypeId` is a solution.
 
 For `data`, I don't have the best idea, because it may be of any type. In order to achieve
-only one Error(Exception), I chose to use `Box<dyn Any>` internally to save it.
+only one Error, I chose to use `Box<dyn Any>` internally to save it.
 
 ## Example
 
-I use the name `Exception` because it is less commonly used in Rust.
-
 ```rust
-use except::Exception;
+use except::ErrorBuilder;
 
 pub struct MyErrorKind;
 
-pub fn foo() -> Result<(), Exception> {
-    Err(Exception::new::<MyErrorKind>("this is my error"))
+pub fn foo() -> except::Result<()> {
+    Err(ErrorBuilder::new::<MyErrorKind>().message("this is my error").build())
 }
 
 pub fn main() {
     if let Err(ex) = foo() {
         if ex.is::<MyErrorKind>() {
             eprintln!("my error detected: {:?}", ex);
-        }
-    }
-}
-```
-
-Image you want to implement the http api response json.
-
-```rust
-use except::Exception;
-
-pub trait Serialize {
-    fn serialize(&self) -> String;
-}
-
-impl Serialize for () {
-    fn serialize(&self) -> String {
-        "null".to_string()
-    }
-}
-
-#[repr(u32)]
-#[non_exhaustive]
-pub enum ApiError {
-    Unauthorized = 401,
-    Forbidden = 403,
-    InternalServerError = 500,
-}
-
-pub struct ApiResponse {
-    code: u32,
-    message: String,
-    data: Box<dyn Serialize>,
-}
-
-pub fn something_failed() -> Result<(), Exception> {
-    Err(Exception::new1::<ApiError>("login required", ApiError::Unauthorized as u32))
-}
-
-pub fn handle_api_exception(ex: Exception) -> ApiResponse {
-    if ex.is::<ApiError>() {
-        ApiResponse {
-            code: ex.get_code(),
-            message: ex.to_string(),
-            data: ex.into_data::<Box<dyn Serialize>>().unwrap(),
-        }
-    } else {
-        ApiResponse {
-            code: 500,
-            message: ex.to_string(),
-            data: Box::new(()),
         }
     }
 }
--- src/lib.rs
+++ src/lib.rs
@@ -12,157 +12,169 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
+#![feature(error_generic_member_access)]
+#![feature(provide_any)]
 #![warn(rust_2018_idioms, missing_docs)]
 #![warn(clippy::dbg_macro, clippy::print_stdout)]
 #![doc = include_str!("../README.md")]
 
 use std::{
-    any::{type_name, Any, TypeId},
-    error::Error,
-    fmt::{self, Debug, Display}, backtrace::{Backtrace, BacktraceStatus}
+    any::{type_name, Any, Demand, TypeId},
+    backtrace::{Backtrace, BacktraceStatus},
+    borrow::Cow,
+    error,
+    fmt::{self, Debug, Display},
+    result,
 };
 
-/// Exception: The only `Error` you deserve.
-pub struct Exception {
-    kind_id: TypeId,
-    kind_name: &'static str,
-    code: u32,
-    message: String,
-    backtrace: Backtrace,
-    data: Option<Box<dyn Any + Send + Sync>>,
-    source: Option<Box<Exception>>,
+struct Type {
+    type_id: TypeId,
+    type_name: &'static str,
 }
 
-impl Exception {
-    /// New [Exception] with kind and message.
-    pub fn new<T: 'static>(message: impl Into<String>) -> Self {
+impl Type {
+    fn new<T: 'static>() -> Self {
         Self {
-            kind_id: TypeId::of::<T>(),
-            kind_name: type_name::<T>(),
-            code: 0,
-            message: message.into(),
-            backtrace: Backtrace::capture(),
-            data: None,
-            source: None,
+            type_id: TypeId::of::<T>(),
+            type_name: type_name::<T>(),
         }
     }
+}
 
-    /// New [Exception] with kind, code and message.
-    pub fn new1<T: 'static>(message: impl Into<String>, code: u32) -> Self {
-        Self {
-            kind_id: TypeId::of::<T>(),
-            kind_name: type_name::<T>(),
-            code,
-            message: message.into(),
-            backtrace: Backtrace::capture(),
-            data: None,
-            source: None,
-        }
-    }
+/// Alias of [Result](result::Result)<T, [Error]>;
+pub type Result<T> = result::Result<T, Error>;
 
-    /// New [Exception] with kind, code, message and data.
-    pub fn new2<T: 'static>(message: impl Into<String>, code: u32, data: impl Any + Send + Sync) -> Self {
-        Self {
-            kind_id: TypeId::of::<T>(),
-            kind_name: type_name::<T>(),
-            code,
-            message: message.into(),
-            backtrace: Backtrace::capture(),
-            data: Some(Box::new(data)),
-            source: None,
-        }
-    }
+/// The only one [`Error`].
+pub struct Error {
+    r#type: Type,
+    sub_type: Type,
+    message: Cow<'static, str>,
+    backtrace: Backtrace,
+    data: Option<Box<dyn Any + Send + Sync>>,
+    source: Option<Box<dyn error::Error + Send + Sync + 'static>>,
+}
 
-    /// New [Exception] with kind, code, message and source.
-    pub fn new3<T: 'static>(message: impl Into<String>, code: u32, source: Exception) -> Self {
-        Self {
-            kind_id: TypeId::of::<T>(),
-            kind_name: type_name::<T>(),
-            code,
-            message: message.into(),
-            backtrace: Backtrace::capture(),
-            data: None,
-            source: Some(Box::new(source)),
-        }
-    }
-
-    /// New [Exception] with kind, code, message, data and source.
-    pub fn new4<T: 'static>(
-        message: impl Into<String>, code: u32, data: impl Any + Send + Sync, source: Exception    ) -> Self {
-        Self {
-                kind_id: TypeId::of::<T>(),
-                kind_name: type_name::<T>(),
-            code,
-                message: message.into(),
-                backtrace: Backtrace::capture(),
-                data: Some(Box::new(data)),
-                source: Some(Box::new(source)),
-        }
-    }
-
-    /// Detect [Exception] is belong to the kind.
+impl Error {
+    /// Detect [Exception] is belong to the type.
+    #[inline]
     pub fn is<T: 'static>(&self) -> bool {
-        self.kind_id == TypeId::of::<T>()
+        self.r#type.type_id == TypeId::of::<T>()
     }
 
-    /// Get kind name.
-    pub fn get_kind_name(&self) -> &'static str {
-        self.kind_name
-    }
-
-    /// Get code.
-    pub fn get_code(&self) -> u32 {
-        self.code
-    }
-
-    /// Get source exception.
-    pub fn get_source(&self) -> Option<&Self> {
-        self.source.as_deref()
-    }
-
-    /// Get backtrace.
-    pub fn get_backtrace(&self) -> &Backtrace {
-        &self.backtrace
+    /// Detect [Error] is belong to the sub type.
+    #[inline]
+    pub fn is_sub<T: 'static>(&self) -> bool {
+        self.sub_type.type_id == TypeId::of::<T>()
     }
 
     /// Get immutable reference data.
-    pub fn get_data<D: 'static>(&self) -> Option<&D> {
-        self .data
+    pub fn data<D: 'static>(&self) -> Option<&D> {
+        self.data
             .as_deref()
             .and_then(|data| (data as &dyn Any).downcast_ref())
     }
 
     /// Convert into data.
     pub fn into_data<D: 'static>(self) -> Option<D> {
-        self .data
+        self.data
             .and_then(|data| data.downcast().ok())
             .map(|data| *data)
     }
 }
 
-impl Debug for Exception {
-    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
-        f.debug_struct("Exception")
-            .field("kind_name", &self.kind_name)
-            .field("message", &self.message)
-            .field("backtrace", &self.backtrace)
-            .finish()
-    }
-}
-
-impl Display for Exception {
+impl Debug for Error {
     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
         write!(f, "{}", &self.message)?;
-        if self.backtrace.status() == BacktraceStatus::Captured {
-            write!(f, "\n{}", &self.backtrace)?;
+
+        if f.alternate() {
+            write!(f, "\nType: {}", &self.r#type.type_name)?;
+            if self.sub_type.type_id != TypeId::of::<()>() {
+                write!(f, "\nSub type: {}", &self.sub_type.type_name)?;
+            }
         }
+
+        if self.backtrace.status() == BacktraceStatus::Captured {
+            write!(f, "\nBacktrace:\n{}", &self.backtrace.to_string())?;
+        }
+
         Ok(())
     }
 }
 
-impl<E: Error + Send + Sync + 'static> From<E> for Exception {
-    fn from(e: E) -> Self {
-        Self::new2::<E>(e.to_string(), 0, e)
+impl Display for Error {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        write!(f, "{}", &self.message)
+    }
+}
+
+impl error::Error for Error {
+    fn source(&self) -> Option<&(dyn error::Error + 'static)> {
+        self.source.as_deref().map(|s| s as _)
+    }
+
+    fn provide<'a>(&'a self, demand: &mut Demand<'a>) {
+        demand.provide_ref(&self.r#type);
+        demand.provide_ref(&self.backtrace);
+    }
+}
+
+/// Builder of [Error].
+pub struct ErrorBuilder {
+    r#type: Type,
+    sub_type: Option<Type>,
+    message: Option<Cow<'static, str>>,
+    data: Option<Box<dyn Any + Send + Sync>>,
+    source: Option<Box<dyn error::Error + Send + Sync + 'static>>,
+}
+
+impl ErrorBuilder {
+    /// Create error builder.
+    pub fn new<T: 'static>() -> Self {
+        Self {
+            r#type: Type::new::<T>(),
+            sub_type: None,
+            message: None,
+            data: None,
+            source: None,
+        }
+    }
+
+    /// Set sub type.
+    pub fn sub_type<T: 'static>(mut self) -> Self {
+        self.sub_type = Some(Type::new::<T>());
+        self
+    }
+
+    /// Set message.
+    pub fn message(mut self, message: impl Into<Cow<'static, str>>) -> Self {
+        self.message = Some(message.into());
+        self
+    }
+
+    /// Set data.
+    pub fn data(mut self, data: impl Any + Send + Sync) -> Self {
+        self.data = Some(Box::new(data));
+        self
+    }
+
+    /// Set source.
+    pub fn source(
+        mut self, source: impl Into<Box<dyn error::Error + Send + Sync + 'static>>,
+    ) -> Self {
+        self.source = Some(source.into());
+        self
+    }
+
+    /// Build error.
+    pub fn build(self) -> Error {
+        Error {
+            r#type: self.r#type,
+            sub_type: self.sub_type.unwrap_or(Type::new::<()>()),
+            message: self.message.unwrap_or_default(),
+            backtrace: Backtrace::capture(),
+            data: self.data,
+            source: self.source,
+        }
     }
 }
 
@@ -172,18 +184,16 @@ mod tests {
 
     trait AssertSendSync: Send + Sync + 'static {}
 
-    impl AssertSendSync for Exception {}
+    impl AssertSendSync for Error {}
 
     #[test]
-    fn test_exception_is() {
+    fn test_exception() {
         pub struct FooError;
-        let e = Exception::new::<FooError>("foo error");
-        assert!(e.is::<FooError>());
-    }
+        let err = ErrorBuilder::new::<FooError>().message("foo error").build();
+        assert!(err.is::<FooError>());
+        assert!(err.is_sub::<()>());
+        assert_eq!(err.to_string(), "foo error");
 
-    #[test]
-    fn test_exception_get_data() {
-        let e = Exception::new2::<()>("foo error", 0, 100usize);
-        assert_eq!(e.get_data::<usize>(), Some(&100usize));
+        dbg!(err);
     }
 }
